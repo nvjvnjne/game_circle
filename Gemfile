004583957b9c0fa47c977afa5177aad32f4a2eb0
nvjvnjne <113250918+nvjvnjne@users.noreply.github.com>
2023-01-11 16:19:35 +0000

    ログイン機能実装

--- Gemfile
+++ Gemfile
@@ -23,7 +23,9 @@ gem 'jbuilder', '~> 2.7'
 # gem 'bcrypt', '~> 3.1.7'
 
 # Use Active Storage variant
-# gem 'image_processing', '~> 1.2'
+
+#画像サイズの変更
+gem 'image_processing', '~> 1.2'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
@@ -54,3 +56,10 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+#デバイスの実装
+gem 'devise'
+#ページネーションの実装
+gem 'kaminari','~> 1.2.1'
+#enum実装
+gem 'enum_help'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  # 会員機能
+  devise_for :users, controllers: {
+    registrations: 'public/registrations',
+    sessions: 'public/sessions',
+    passwords: 'public/passwords'
+  }
+  # トップページ
+  root to: 'public/homes#top'
+  # 利用者側
+  scope module: :public do
+    # ユーザー実装
+    resources :users, only: [:index, :show, :edit]
+  end
+
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sessions: 'admin/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
